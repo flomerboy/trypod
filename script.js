b4e3c4fe3d4cf17f8c2f1793635a496448d11abe
flomerboy <rmather@risd.edu>
2015-05-05 02:49:16 -0400

    styling the landing page

--- script.js
+++ script.js
@@ -7,23 +7,38 @@ $(document).ready(function(){
     	return Math.floor(low2 + (high2 - low2) * (value - low1) / (high1 - low1));
 	};
 
+	function map_range_decimal(value, low1, high1, low2, high2) {
+    	return (low2 + (high2 - low2) * (value - low1) / (high1 - low1));
+	};
+
 	readHeight = function(){      					// Height of window (visible part).
 		scrollHeight = $(window).scrollTop();			//this value starts high and ends low
 		console.log("scrollHeight = " + scrollHeight);
-		R = map_range(scrollHeight, 0, headerHeight, 0, 100);
-		G = map_range(scrollHeight, 0, headerHeight, 133, 165);
-		B = map_range(scrollHeight, 0, headerHeight, 200, 250);
-		console.log("RGB = ("  + R + ", " + G + ", " + B + ")");	
+		R = map_range(scrollHeight, 0, headerHeight, 83, 33);
+		G = map_range(scrollHeight, 0, headerHeight, 200, 150);
+		B = map_range(scrollHeight, 0, headerHeight, 255, 243);
+		A = map_range_decimal(scrollHeight, 0, headerHeight, 0, 1);
+		console.log("RGB = ("  + R + ", " + G + ", " + B + ", " + A +")");	
 	};
 
 	draw = function(){
-		$("div.header").css("background-color","rgb(" + R + ", " + G + ", " + B + ")");
-		actualColor = $("div.header").css("background-color	");
+		$("div.header").css("background-color","rgba(" + R + ", " + G + ", " + B + ", " + A + ")");
+		actualColor = $("div.header").css("background-color");
 		console.log(actualColor);
+		if (scrollHeight>headerHeight) {
+			$("ul#nav").css("position","fixed");
+			$("ul#nav").css("background-color","rgba(33,150,243,.9)");
+			$("li ul").css("background-color","rgba(33,150,243,.9)");
+			console.log("nav");
+		} else {
+			$("ul#nav").css("position","absolute");
+			$("ul#nav").css("background-color","rgba(0,0,0,0)");
+			$("li ul").css("background-color","rgba(0,0,0,0)");
+		}
 	};
 	
-	readHeight;
-	draw;
+	readHeight();
+	draw();
 
 	$(document).scroll(function(){
 		readHeight();
