c6c0879c3db7493f8df2273fbb700caebd4b96f2
flomerboy <rmather@risd.edu>
2015-05-07 20:37:51 -0400

    added some

--- scriptGetStarted.js
+++ scriptGetStarted.js
@@ -12,30 +12,15 @@ $(document).ready(function(){
 	};
 
 	readHeight = function(){      					// Height of window (visible part).
-		scrollHeight = $(window).scrollTop();			//this value starts high and ends low
-		console.log("scrollHeight = " + scrollHeight);
-		R = map_range(scrollHeight, 0, headerHeight, 83, 33);
-		G = map_range(scrollHeight, 0, headerHeight, 200, 150);
-		B = map_range(scrollHeight, 0, headerHeight, 255, 243);
-		A = map_range_decimal(scrollHeight, 0, headerHeight, 0, 1);
-		console.log("RGB = ("  + R + ", " + G + ", " + B + ", " + A +")");	
+
 	};
 
 	draw = function(){
-		$("div.header").css("background-color","rgba(" + R + ", " + G + ", " + B + ", " + A + ")");
-		actualColor = $("div.header").css("background-color");
-		console.log(actualColor);
-		if (scrollHeight>headerHeight) {
+
 			$("ul#nav").css("position","fixed");
 			$("ul#nav").css("background-color","rgba(33,150,243,.9)");
 			$("li ul").css("background-color","rgba(33,150,243,.9)");
-			console.log("nav");
-		} else {
-			$("ul#nav").css("position","absolute");
-			$("ul#nav").css("background-color","rgba(0,0,0,0)");
-			$("li ul").css("background-color","rgba(0,0,0,0)");
-		}
-	};
+	}
 	
 	readHeight();
 	draw();
